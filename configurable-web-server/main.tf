6beb12b38b157fec2bb672df311015b5e372eef5
JFMajer <jakubmajer@live.com>
2023-02-22 17:51:26 +0100

    Destroy

--- configurable-web-server/main.tf
+++ configurable-web-server/main.tf
@@ -53,3 +53,8 @@ output "public_ip" {
   value = aws_instance.example.public_ip
   description = "The public IP address of the web server"
 }
+
+output "url" {
+  value = "http://${aws_instance.example.public_ip}:${var.server_port}"
+  description = "The URL of the web server"
+}
