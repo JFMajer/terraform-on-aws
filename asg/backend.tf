c8f6296494ce8000bca418b5065b468ae0a71d59
JFMajer <jakubmajer@live.com>
2023-02-24 19:47:54 +0100

    fixed relative path to module

--- asg/backend.tf
+++ asg/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "#{S3_BUCKET}#"
-    key    = "asg/services/webserver-cluster/terraform.tfstate"
+    key    = "asg/terraform.tfstate"
     region = "#{AWS_REGION}#"
     dynamodb_table = "#{DYNAMO_TABLE}#"
     encrypt = true
