ec953ecbb4aaccd838800c13fb0025fc31e1ecdf
JFMajer <jakubmajer@live.com>
2023-02-24 19:27:25 +0100

    fixed relative path to module

--- asg/main.tf
+++ asg/main.tf
@@ -3,13 +3,13 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-    source = "../../modules/asg/services/webserver-cluster"
+    source = "../modules/asg/services/webserver-cluster"
     cluster_name = "webserver-cluster-#{ENV}#"
     db_address = module.mysql_rds.address
     db_port = module.mysql_rds.port
 }
 
 module "mysql_rds" {
-    source = "../../modules/data-stores/mysql-rds"
+    source = "../modules/data-stores/mysql-rds"
     rds_suffix = "-#{ENV}#"
 }
