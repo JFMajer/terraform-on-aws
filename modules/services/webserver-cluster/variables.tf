d0ffa7efd936557997b0ec47e1e12e75f41bdd2b
JFMajer <jakubmajer@live.com>
2023-02-24 20:02:39 +0100

    fixed variables passed to module

--- modules/services/webserver-cluster/variables.tf
+++ modules/services/webserver-cluster/variables.tf
@@ -13,3 +13,8 @@ variable "db_port" {
     description = "The port of the database"
     type = number
 }
+
+variable "cluster_name" {
+    description = "Name of the cluster"
+    type        = string
+}
