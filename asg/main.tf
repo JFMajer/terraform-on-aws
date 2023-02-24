75bc4b7c6dfa9710d2ae74531cf07d81035258fe
JFMajer <jakubmajer@live.com>
2023-02-24 19:57:48 +0100

    fixed variables passed to module

--- asg/main.tf
+++ asg/main.tf
@@ -4,12 +4,12 @@ provider "aws" {
 
 module "webserver_cluster" {
     source = "../modules/services/webserver-cluster"
-    cluster_name = "webserver-cluster-#{ENV}#"
+    cluster_name = var.cluster_name
     db_address = module.mysql_rds.address
     db_port = module.mysql_rds.port
 }
 
 module "mysql_rds" {
     source = "../modules/data-stores/mysql-rds"
-    rds_suffix = "-#{ENV}#"
+    rds_suffix = var.rds_suffix
 }
--- asg/variables.tf
+++ asg/variables.tf
@@ -1,10 +1,11 @@
 variable "cluster_name" {
     description = "Name of the cluster"
     type        = string
-    default = "webserver-cluster"
+    default = "webserver-cluster-#{ENV}#"
 }
 
 variable "rds_suffix" {
     description = "Suffix to append to the RDS instance name"
     type        = string
+    default = "-#{ENV}#"
 }
