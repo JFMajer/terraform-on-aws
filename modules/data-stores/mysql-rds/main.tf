c4f0844dcd89965cffac69b0da734f31cea0c96e
JFMajer <jakubmajer@live.com>
2023-02-25 13:23:03 +0100

    changes to rds name and information that website outputs

--- modules/data-stores/mysql-rds/main.tf
+++ modules/data-stores/mysql-rds/main.tf
@@ -1,5 +1,5 @@
 resource "aws_db_instance" "rds_mysql" {
-    identifier = "rds-mysql-${var.cluster_name}${var.rds_suffix}-${random_string.random.result}"
+    identifier = "rds-mysql-${var.cluster_name}-${random_string.random.result}"
     allocated_storage = 20
     engine = "mysql"
     instance_class = "db.t3.micro"
--- modules/services/webserver-cluster/user-data.sh
+++ modules/services/webserver-cluster/user-data.sh
@@ -1,9 +1,21 @@
 #!/bin/bash
 
+echo 'INSTANCE_ID="$(curl http://169.254.169.254/latest/meta-data/instance-id)"
+PRIVATE_IP="$(hostname -I)"
+PUBLIC_IP="$(curl http://169.254.169.254/latest/meta-data/public-ipv4)"
+PUBLIC_HOSTNAME="$(curl http://169.254.169.254/latest/meta-data/public-hostname)"
+
 cat > index.html <<EOF
 <h1>Hello, World</h1>
 <p>DB address: ${db_address}</p>
 <p>DB port: ${db_port}</p>
+<hr>
+<p>Loadbalances to EC2 instance of:
+<ul>
+		<li><strong>Instance ID</strong>: $INSTANCE_ID</li>
+        <li><strong>Private IP</strong>: $PRIVATE_IP</li>
+        <li><strong>Public IP</strong>: $PUBLIC_IP</li>
+        <li><strong>Public DNS</strong>: $PUBLIC_HOSTNAME</li>
 EOF
 
 nohup busybox httpd -f -p "${server_port}" &
