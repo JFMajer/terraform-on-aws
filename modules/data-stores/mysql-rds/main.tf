69a415ca5cbcd2e5b6489f0ce6314a8520aefb69
JFMajer <jakubmajer@live.com>
2023-02-28 18:13:34 +0100

    changed vpc cidr

--- modules/data-stores/mysql-rds/main.tf
+++ modules/data-stores/mysql-rds/main.tf
@@ -10,8 +10,6 @@ resource "aws_db_instance" "rds_mysql" {
     username = var.rds_username
     password = var.rds_password
 
-    create_db_subnet_group = true
-    subnet_ids = var.subnet_ids
 }
 
 
@@ -20,4 +18,9 @@ resource "random_string" "random" {
     length = 4
     special = false
     upper = false
+}
+
+resource "aws_db_subnet_group" "rds_mysql" {
+    name = "rds-mysql-${var.cluster_name}-subnetgroup"
+    subnet_ids = var.subnet_ids
 }
