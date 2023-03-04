94bc66b2f1928a269a805da8db4851ed6d77c550
JFMajer <jakubmajer@live.com>
2023-03-04 12:33:38 +0100

    changed module version to use

--- asg/main.tf
+++ asg/main.tf
@@ -20,7 +20,7 @@ provider "aws" {
 }
 
 module "webserver_cluster" {
-    source = "git::github.com/JFMajer/terraform-aws-asg-module?ref=v0.1.9"
+    source = "git::github.com/JFMajer/terraform-aws-asg-module?ref=v0.0.1"
     cluster_name = var.cluster_name
     db_address = module.mysql_rds.address
     db_port = module.mysql_rds.port
