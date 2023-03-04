0bd1d422095eb3b73365bb3f25d0bc4b3bec3825
JFMajer <jakubmajer@live.com>
2023-03-04 11:59:05 +0100

    create cert and validation

--- asg/main.tf
+++ asg/main.tf
@@ -85,10 +85,10 @@ resource "aws_acm_certificate" "alb_cert" {
 resource "aws_route53_record" "alb_cert_validation" {
   provider = aws.dns
   for_each = {
-    for dvo in aws_acm_certificate.alb_cert : dvo.domain_name => {
-      name = dvo.resource_record_name
-      type = dvo.resource_record_type
-      value = dvo.resource_record_value
+    for dvo in aws_acm_certificate.alb_cert.domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
     }
   }
   allow_overwrite = true
